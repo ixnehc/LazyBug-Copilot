2f04981c6e5437f9d21738b7c934e9a6f2c92b08
Chen Xi <xi.chen@ubisoft.com>
2026-07-01 20:05:54 +0800

    Remove Interfaces, Lib_Common, Lib_TreeSitterCSharp from tracking

--- Interfaces/GameSystem/IGameSystemDefines.h
+++
@@ -1,9 +0,0 @@
-/********************************************************************
-	created:	13:4:2009   15:33
-	filename: 	d:\IxEngine\Interfaces\GameLogic\IGameLogicDefines.h
-	author:		chenxi
-	
-	purpose:	game logic defines
-*********************************************************************/
-#pragma once
-
--- Interfaces/WorldSystem/assetcore/assetctrl/NotifyArgs.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdh.h"
-#include "NotifyArgs.h"
-
-// Mouse
-IMPLEMENT_CLASS(MouseNotifyArgs)
-
-// Key
-IMPLEMENT_CLASS(KeyNotifyArgs)
